1a83b0cff73c8372db4e6b95f55be8f3253ddec6
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-10-04 21:28:39 -0400

    feat: :sparkles: add method delete in icon

--- src/components/AddPaymentForm.tsx/index.tsx
+++ src/components/AddPaymentForm.tsx/index.tsx
@@ -1,13 +1,25 @@
 import { PlusIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useFetchPayments } from "../../hooks/useFetchPayments";
+import { Payment } from "../../types/Payments";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
-import { useForm } from "react-hook-form";
-import { Payment } from "../../types/Payments";
-import { useFetchPayments } from "../../hooks/useFetchPayments";
 
 const AddPaymentForm = () => {
-  const { register, handleSubmit } = useForm<Payment>();
+  const { register, handleSubmit } = useForm<Payment>({
+    defaultValues: {
+      nameEmployee: "",
+      salary: "",
+      store: "",
+      datePayment: "",
+      bonification: "",
+      overTime: "",
+      advanceMoney: "",
+      cardLoan: "",
+      discounts: "",
+    },
+  });
   const { calculateSalaryTotal, sendPaymentData, createPayment } =
     useFetchPayments();
   const onSubmit = async (data: Payment) => {
@@ -17,7 +29,30 @@ const AddPaymentForm = () => {
       window.location.reload();
     });
   };
-  //   const calculateSalaryTotal =
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.currentTarget;
+    let value = input.value.replace(/[^0-9.,]/g, "");
+
+    // Remove ponto se existir antes da vírgula
+    value = value.replace(/\./g, "").replace(",", ".");
+
+    // Converter para número
+    const numericValue = Number(value);
+
+    register(e.currentTarget.name as keyof Payment).onChange(e);
+
+    if (!isNaN(numericValue)) {
+      const formattedValue = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        maximumFractionDigits: 2,
+      }).format(numericValue);
+
+      input.value = formattedValue;
+    }
+  };
+
   //     Number(data.salary) -
   //     Number(data.advanceMoney) -
   //     Number(data.cardLoan) -
@@ -50,46 +85,92 @@ const AddPaymentForm = () => {
       </CardHeader>
       <CardContent>
         <div className="space-y-4">
-          <Input placeholder="Loja" {...register("store")} />
-          <Input
-            placeholder="Nome do Funcionário"
-            {...register("nameEmployee")}
-          />
-          <Input
-            type="number"
-            placeholder="Salario Bruto"
-            {...register("salary")}
-          />
-          <Input
-            type="date"
-            placeholder="Data do Pagamento"
-            {...register("datePayment")}
-          />
-          <Input
-            type="number"
-            placeholder="Desconto INSS"
-            {...register("discounts")}
-          />
-          <Input
-            type="number"
-            placeholder="Desconto FGTS"
-            {...register("discounts")}
-          />
-          <Input
-            type="number"
-            placeholder="Desconto Cartão"
-            {...register("cardLoan")}
-          />
-          <Input
-            type="number"
-            placeholder="Desconto Adiantamento"
-            {...register("advanceMoney")}
-          />
-          <Input
-            type="number"
-            placeholder="Valor das Horas Extras"
-            {...register("overTime")}
-          />
+          <label className="text-xs">
+            Loja
+            <Input
+              placeholder="Shopping"
+              {...register("store")}
+              id="loja"
+              className="mb-2"
+            />
+          </label>
+          <label className="text-xs">
+            Funcionário
+            <Input
+              placeholder="Nome do Funcionário"
+              {...register("nameEmployee")}
+              className="mb-2"
+            />
+          </label>
+          <label className="text-xs">
+            Salário
+            <Input
+              type="text"
+              placeholder="Salario Bruto"
+              {...register("salary")}
+              className="mb-2"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label className="text-xs">
+            Data
+            <Input
+              type="date"
+              placeholder="Data do Pagamento"
+              {...register("datePayment")}
+              className="mb-2"
+            />
+          </label>
+
+          <label className="text-xs">
+            Desconto INSS / FGTs
+            <Input
+              type="number"
+              placeholder="Desconto INSS"
+              {...register("discounts")}
+              className="mb-2"
+            />
+          </label>
+
+          <label className="text-xs">
+            Desconto Cartão
+            <Input
+              type="number"
+              placeholder="Desconto Cartão"
+              {...register("cardLoan")}
+              className="mb-2"
+            />
+          </label>
+
+          <label className="text-xs">
+            Desconto Adiantamento
+            <Input
+              type="number"
+              placeholder="Desconto Adiantamento"
+              {...register("advanceMoney")}
+              className="mb-2"
+            />
+          </label>
+          <label className="text-xs">
+            Bonificação / Salário Família
+            <Input
+              type="number"
+              placeholder="Bonificação / Salário Família"
+              {...register("bonification")}
+              className="mb-2"
+            />
+          </label>
+
+          <label className="text-xs">
+            Horas Extras
+            <Input
+              type="number"
+              placeholder="Valor das Horas Extras"
+              {...register("overTime")}
+              className="mb-2"
+            />
+          </label>
         </div>
         <Button className="w-full mt-4 button" onClick={handleSubmit(onSubmit)}>
           <PlusIcon className="w-4 h-4 mr-2" /> Adicionar Pagamento
